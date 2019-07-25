08aa235b8dc6d882ff8942ed67208d7aea7a0edb
datagutten <datagutten@datagutten.net>
2019-07-25 22:05:26 +0200

    Add more options

--- tests/Test.php
+++ tests/Test.php
@@ -10,7 +10,15 @@ class Test extends TestCase
 
     function testLdap()
     {
-        $ldap = ldap_connect('127.0.0.1', 3389);
-        ldap_bind($ldap, 'admin', 'test');
+        $ldap = ldap_connect('ldap://127.0.0.1', 3389);
+
+        ldap_set_option($ldap, LDAP_OPT_PROTOCOL_VERSION, 3);
+        ldap_set_option($ldap, LDAP_OPT_NETWORK_TIMEOUT, 1);
+        if (!ldap_set_option($ldap, LDAP_OPT_REFERRALS, 0))
+            throw new Exception('Failed to set opt referrals to 0');
+
+        ldap_bind($ldap, 'cn=admin,dc=example,dc=com', 'test');
+
+
     }
 }
